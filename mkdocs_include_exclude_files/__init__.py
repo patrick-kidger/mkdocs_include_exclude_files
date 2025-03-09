4de3dec3dac0ab31399307161710a6fca6a20cfa
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2025-03-09 12:49:03 +0100

    Fix warnings with mkdocs 1.6.1

--- mkdocs_include_exclude_files/__init__.py
+++ mkdocs_include_exclude_files/__init__.py
@@ -1,10 +1,10 @@
-from mkdocs import utils
+from mkdocs.config.defaults import MkDocsConfig
 from mkdocs.config.config_options import Type
 from mkdocs.plugins import BasePlugin
-from mkdocs.structure.files import File
+from mkdocs.structure.files import File, Files, InclusionLevel
 
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 
 class IncludeExcludePlugin(BasePlugin):
@@ -13,17 +13,18 @@ class IncludeExcludePlugin(BasePlugin):
         ("exclude", Type(list, default=[])),
     )
 
-    def on_files(self, files, config):
+    def on_files(self, files: Files, /, *, config: MkDocsConfig) -> Files:
         plugin_config = config["plugins"]["include_exclude_files"].config
         for filename in plugin_config["include"]:
-            files.append(
-                File(
-                    filename,
-                    config["docs_dir"],
-                    config["site_dir"],
-                    config["use_directory_urls"],
-                )
+            file = File(
+                filename,
+                config["docs_dir"],
+                config["site_dir"],
+                config["use_directory_urls"],
+                inclusion=InclusionLevel.INCLUDED
             )
+            files.remove(file)
+            files.append(file)
         for to_exclude in plugin_config["exclude"]:
             for file in list(files):
                 if file.src_path.startswith(to_exclude):
